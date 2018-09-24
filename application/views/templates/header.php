3f6cddb8bd760bf9b05b2be401f35f4e434c7870
nishida1 <nishidanexus6@gmail.com>
2018-09-24 23:36:48 +0900

    mod url #2

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,3 +4,8 @@
 	</head>
 	<body>
 
+	<ul class="nav navbar-nav">
+	  <li><a href="<?php echo base_url(); ?>">Home</a></li>
+	  <li><a href="<?php echo base_url(); ?>about">About</a></li>
+	</ul>
+       
