1526f874a9f69ff82a37304b2c3718557e8a20a0
nishida1 <nishidanexus6@gmail.com>
2018-09-28 03:27:22 +0900

    adminlte #2

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,2 +1,16 @@
-	</body>
+</section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2018 Company</strong> All rights reserved.
+  </footer>
+
+<script src="<?php echo base_url(); ?>assets/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/dist/js/adminlte.min.js"></script>
+
+</body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,12 +1,66 @@
+<!DOCTYPE html>
 <html>
-	<head>
-		<title>citest</title>
-	</head>
-	<body>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>CRUDTest</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/skins/skin-blue.min.css">
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+  <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
 
-	<ul class="nav navbar-nav">
-	  <li><a href="<?php echo base_url(); ?>">Home</a></li>
-	  <li><a href="<?php echo base_url(); ?>about">About</a></li>
-		<li><a href="<?php echo base_url(); ?>posts">Posts</a></li>
-	</ul>
-       
+  <!-- Main Header -->
+  <header class="main-header">
+    <a href="index.html" class="logo">
+      <span class="logo-mini">CT</span>
+      <span class="logo-lg"><b>CRUD</b>Test</span>
+    </a>
+
+    <!-- Header Navbar -->
+    <nav class="navbar navbar-static-top" role="navigation">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+    </nav>
+  </header>
+
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+      <!-- Sidebar Menu -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li><a href="<?php echo base_url(); ?>"><i class="fa fa-home"></i> <span>Home</span></a></li>
+        <li><a href="<?php echo base_url(); ?>about"><i class="fa fa-pencil"></i> <span>About</span></a></li>
+        <li><a href="<?php echo base_url(); ?>posts"><i class="fa fa-list"></i> <span>Post</span></a></li>
+        <li class="active"><a href="#"><i class="fa fa-sticky-note-o"></i> <span>Note</span></a></li>
+      </ul>
+    </section>
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Title
+        <small>description</small>
+      </h1>
+    </section>
+    
+    <!-- Main content -->
+    <section class="content container-fluid">
